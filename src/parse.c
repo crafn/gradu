adcbed823896e0db5e7dae7ad1a2958686b4de55
crafn <tuukka.pensala@gmail.com>
2016-03-13 22:42:59 +0200

    Fixed argument list parsing

--- src/parse.c
+++ src/parse.c
@@ -1019,8 +1019,12 @@ QC_INTERNAL QC_Bool parse_arg_list(Parse_Ctx *ctx, QC_Array(QC_AST_Node_Ptr) *re
 		if (cur_tok(ctx)->type == QC_Token_comma)
 			advance_tok(ctx);
 
+		/* Allow (foo, bar,) */
+		if (cur_tok(ctx)->type == ending)
+			break;
+
 		/* Normal expression */
-		if (!parse_expr(ctx, (QC_AST_Node**)&arg, 0, NULL, QC_false))
+		if (!parse_expr(ctx, (QC_AST_Node**)&arg, qc_biop_prec(QC_Token_comma) + 1, NULL, QC_false))
 			goto mismatch;
 
 		qc_push_array(QC_AST_Node_Ptr)(ret, arg);
